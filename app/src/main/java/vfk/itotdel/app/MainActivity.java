08c28f3ab2b712ef9e16ea0ee449920f561d4f06
opushnev <opushnev@inbox.ru>
2017-10-25 20:56:20 +0300

    Initial commit

---
+++ app/src/main/java/vfk/itotdel/app/MainActivity.java
@@ -0,0 +1,14 @@
+package vfk.itotdel.app;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
